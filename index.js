99d55858d51887f4426eb23638e2720307777143
Nandini Deodia <120674361+nandinideodia@users.noreply.github.com>
2023-07-04 21:56:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+let randomImageSource = "images/dice"+ randomNumber1 + ".png"
+let image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource)
+
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png"
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
